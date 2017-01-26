aa2dc3b5ecea8541c826e5251e2da2a5b04ce004
jeongwooha <jwha@stanford.edu>
2017-01-26 13:26:54 -0800

    edited v timer

--- collectHifiStats.js
+++ collectHifiStats.js
@@ -1,7 +1,6 @@
-
-
+// collectHifiStats.js
 // Last updated 1/26/17 by JeongWoo Ha
-
+// for HiFi Study
 
 var ENDPOINT_URL = "https://qcy5nql2a4.execute-api.us-west-2.amazonaws.com/prod"; // NEW URL!
 
